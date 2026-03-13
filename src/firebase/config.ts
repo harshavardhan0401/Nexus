c45c5be9b262a695e1099e1c6f6daa34c4765404
harsha uddangi <harshauddangi@gmail.com>
2026-03-13 08:14:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7271718336-14925",
+  "appId": "1:934189134005:web:0fa598fed6276e3604cefc",
+  "apiKey": "AIzaSyBJq-yXqzh5hzZ7M7YlE0O34aShxuMIGjw",
+  "authDomain": "studio-7271718336-14925.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "934189134005"
+};
